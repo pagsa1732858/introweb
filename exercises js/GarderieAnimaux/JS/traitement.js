9496d910497c2855757e99afd75d2a3579e86c43
PagSa1732858 <pagsa1732858@etu.cegepjonquiere.ca>
2018-11-08 22:01:43 -0500

    site1

--- exercises js/GarderieAnimaux/JS/traitement.js
+++ exercises js/GarderieAnimaux/JS/traitement.js
@@ -1,26 +1,69 @@
 function btnCalculer_onclick()
 {
-    var nbrJr,vete,chien,chat,tarif,animal;
+    var nbrJr,vete,chien,chat,tarif,animal,rabais,serT,nbHeure,tauxH;
 
-    vete = parseInt(document.getElementById("lstVeterinaire").value);
+    vete = parseInt(document.getElementById("lstVeterinaire").check);
     nbrJr = parseInt(document.getElementById("txtNbreJours").value);
-    chien = parseInt(document.getElementById("radChien").value);
-    chat = parseInt(document.getElementById("radChat").value);
+    nbHeure = parseInt(document.getElementById("txtHeure").value);
+
     if(document.getElementById("radChien").checked == true)
     {
-        tarif=21.28*nbrJr;
+        tarif=21.28;
+        document.getElementById("imgChien").src= "img/Chien.jpg";
     }
     else
     {
-        tarif=19.49*nbrJr;
+        tarif=19.49;
+        document.getElementById("imgChat").src= "img/Chat.jpg";
     }
+    tarif=tarif*nbrJr;
+
     if(document.getElementById("chkServ").checked == true)
     {
-        tarif =tarif +5;
+        serT =5;
     }
     else
     {
-        tarif=tarif+0;
+        serT=+0;
     }
-console.log("votre tarif pour la garderie est de:"+tarif);
+    tarif =tarif +serT;
+    if(nbrJr>=1 && nbrJr<5)
+    {
+        rabais=0;
+    }
+    else
+    {
+        if(nbrJr>=5 && nbrJr<10 )
+        {
+        rabais=5;
+        }
+        else
+        {
+            if(nbrJr>=10 && nbrJr<30)
+            {
+                rabais=10
+            }
+            else
+            {
+               rabais=15
+            }
+        }
+    }
+    switch(vete)
+    {
+        case "Audrey Bouchard":
+            tauxH=25;
+            break;
+    }
+
+
+    rabais=tarif-(tarif*rabais/100);
+    console.log("votre tarif pour la garderie est de:"+tarif);
+    console.log("votre prix avec le rabais est:"+rabais);
+    console.log("le veterinaire a un taux horaire de:"+tauxH);
 }
+
+
+
+
+
