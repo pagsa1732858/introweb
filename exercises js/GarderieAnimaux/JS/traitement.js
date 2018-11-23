073f23400ac6c75eb91d456ffd2bd686f19a7722
PagSa1732858 <pagsa1732858@etu.cegepjonquiere.ca>
2018-11-23 09:19:49 -0500

    site1

--- exercises js/GarderieAnimaux/JS/traitement.js
+++ exercises js/GarderieAnimaux/JS/traitement.js
@@ -1,11 +1,47 @@
 function btnCalculer_onclick()
 {
-    var nbrJr,vete,tarif,rabais,serT,nbHeure,tauxH;
+
+    if( valideChampsObligatoire()===true)
+    {
+        saisir();
+    }
+}
+
+function valideChampsObligatoire()
+{
+    var tabId =new Array("txtNbreJours","txtHeure","txtNom","txtTel");
+    var cpt=0;
+    var valide=false;
+    for(i=0; i<tabId.length; i++)
+    {
+        if(valideExiste(tabId[i]))
+        {
+            cpt++;
+        }
+    }
+    if(4 === cpt)
+    {
+        valide=true;
+    }
+    return valide
+}
+
+function saisir()
+{
+    var nbrJr,vete,tarif,rabais,serT,nbHeure,tauxH,nomC,tel;
 
     vete = document.getElementById("lstVeterinaire").value;
     nbrJr = parseInt(document.getElementById("txtNbreJours").value);
     nbHeure = parseInt(document.getElementById("txtHeure").value);
+    nomC = document.getElementById("txtNom").value;
+    tel = parseInt(document.getElementById("txtNom").value);
+    total = calculer(nbrJr,nomC,vete,tarif,rabais,serT,nbHeure,tauxH);
 
+
+}
+function calculer(nbrJr,nomC,vete,tarif,rabais,serT,nbHeure,tauxH,tel)
+{
+    var total;
     if(document.getElementById("radChien").checked == true)
     {
         tarif=21.28;
@@ -35,7 +71,7 @@ function btnCalculer_onclick()
     {
         if(nbrJr>=5 && nbrJr<10 )
         {
-        rabais=5;
+            rabais=5;
         }
         else
         {
@@ -45,7 +81,7 @@ function btnCalculer_onclick()
             }
             else
             {
-               rabais=15
+                rabais=15
             }
         }
     }
@@ -65,17 +101,22 @@ function btnCalculer_onclick()
             break;
     }
 
- tauxH = tauxH*nbHeure;
+    tauxH = tauxH*nbHeure;
     rabais=tarif-(tarif*rabais/100);
-
-
     document.getElementById("lblMessage").innerHTML ="votre tarif pour la garderie est de:"+tarif+" votre prix avec le rabais est:"+rabais+ " le prix pour les heures du veterinaire est:"+tauxH;
-
-
-
+    return total;
 }
-
-
-
-
-
+function valideExiste(nomId)
+{
+    var valide=false;
+    if(document.getElementById(nomId).value ==="")
+    {
+        document.getElementById(nomId).style.backgroundColor = "red";
+    }
+    else
+    {
+        document.getElementById(nomId).style.backgroundColor = "white";
+        valide=true;
+    }
+    return valide;
+}
